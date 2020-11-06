8cede5fca00eba76417f2e8d024a206d0d251011
Erik Sanchez <KireX91@gmail.com>
2020-11-06 09:23:24 -0800

    Added Settings file
    Added a settings file for easier and faster access to game settings

--- space_shooter.py
+++ space_shooter.py
@@ -1,11 +1,14 @@
 import sys  #Used to exit the game when the player quits
 import pygame  #Contains the functionality needed to make a game
 
+from settings import Settings
+
 def run_game():
     pygame.init()  #Initializes the background settings that Pygame needs to work properly
-    screen=pygame.display.set_mode((1200,800))  #Create display window to draw all of the game's elements
+    game_settings=Settings()
+    screen=pygame.display.set_mode((game_settings.screen_width,game_settings.screen_height))  #Create display window to draw all of the game's elements
     pygame.display.set_caption("Space Shooter")
-    background_color=(230,230,230)
+
     #Main loop of the game
     while True:
         #  Queue for keyboard/mouse actions
@@ -13,7 +16,7 @@ def run_game():
             if event.type==pygame.QUIT:  #Check if we press the close button on the top corner of the display window
                 sys.exit()
 
-        screen.fill(background_color)
+        screen.fill(game_settings.background_color)
         #  Making the screen visible
         pygame.display.flip()
 
