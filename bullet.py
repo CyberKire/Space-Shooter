998334a1dfead4adcdbf8d6a3c216510976ea90d
Erik Sanchez <KireX91@gmail.com>
2020-11-10 12:17:43 -0800

    Deleted Old Bullets
    Old bullets that no longer appear on the screen are deleted.

--- bullet.py
+++ bullet.py
@@ -9,14 +9,14 @@ class Bullet(Sprite):
 
         #Creating a bullet rect at 0,0
         self.rect=pygame.Rect(0,0,game_settings.bullet_width,game_settings.bullet_height) #  Build rect from scratch using the pygame.Rect() class
-        self.rect.centerx=ship.rect.centerx
-        self.rect.top=ship.rect.top
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
 
         #Setting bullet position as a decimal
-        self.y=float(self.rect.y)
+        self.y = float(self.rect.y)
 
-        self.color=game_settings.bullet_color
-        self.speed_factor=game_settings.bullet_speed_factor
+        self.color = game_settings.bullet_color
+        self.speed_factor = game_settings.bullet_speed_factor
 
     def update(self):
         #Moving the bullet up
